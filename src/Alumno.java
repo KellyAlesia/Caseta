9c42ec297f0dc779d09805c98f3c6c9270c99650
Fortunato <33538533+fortunatoramirez@users.noreply.github.com>
2019-02-11 19:00:18 -0800

    Modifiqué el archivo Alumno.java

--- src/Alumno.java
+++ src/Alumno.java
@@ -214,7 +214,7 @@ public class Alumno extends javax.swing.JFrame {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -241,6 +241,9 @@ public class Alumno extends javax.swing.JFrame {
             }
         });
     }
+
+
+    /* Sólo agregué este comentario para que se vea la modificación del código en GitHub */
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField carrera;
